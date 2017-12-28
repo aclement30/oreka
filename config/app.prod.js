fa67a7d8f4e15f89e449d03e0130a279e5827997
Alexandre Clément <web@alexandreclement.com>
2017-12-28 13:01:23 -0500

    Revert "Update endpoint url in production"
    This reverts commit 26552ed4a57acb9e5a88c5c2bcff201417cb8a52.

--- config/app.prod.js
+++ config/app.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  _OREKA_API_ENDPOINT_: JSON.stringify('http://oreka-api.aclement.com'),
+  _OREKA_API_ENDPOINT_: JSON.stringify('http://oreka.aclement.com/api'),
   _OREKA_GOOGLE_CLIENT_ID_: JSON.stringify(process.env.OREKA_GOOGLE_CLIENT_ID),
 };
