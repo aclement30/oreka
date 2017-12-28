276f02f23f555061abd37d03ace3733f0e3f2c2c
Alexandre Clément <web@alexandreclement.com>
2017-12-27 21:35:16 -0500

    Fix config files

--- config/app.prod.js
+++ config/app.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  _OREKA_API_ENDPOINT_: JSON.stringify('http://localhost:3000'),
-  _OREKA_GOOGLE_CLIENT_ID_: JSON.stringify('529258741380-bdvaq5l3bd3o1a60opija08s283dbpp4.apps.googleusercontent.com'),
+  _OREKA_API_ENDPOINT_: JSON.stringify('http://oreka.aclement.com/api'),
+  _OREKA_GOOGLE_CLIENT_ID_: JSON.stringify(process.env.OREKA_GOOGLE_CLIENT_ID),
 };
