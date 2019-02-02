77ad9363fc570e29a556b0430ac5edb2da94f88d
Johann Boutet <contact@johannboutet.com>
2019-02-01 16:22:41 -0800

    Temporary fix for avatar bug

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
-
-import { User } from '../models/user.model';
+import { User } from 'app/models/user.model';
 
 @Component({
   selector: 'app-avatar',
-  template: `<div [style.background-color]="user.color"><span>{{ user.initials }}</span></div>`,
-  styleUrls: ['./avatar.component.scss']
+  template: `
+    <div *ngIf="user" [style.background-color]="user.color"><span>{{ user.initials }}</span></div>
+  `,
+  styleUrls: ['./avatar.component.scss'],
 })
 
 export class AvatarComponent {
